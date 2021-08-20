0637d55653337f1ed04aa993b4c07b5317ac71a3
MaxAtoms <7847075+MaxAtoms@users.noreply.github.com>
2021-08-20 22:18:40 +0200

    Split DI setup into options and service binding

---
+++ src/RepoStatusTable/DependencyInjection/Bindings.cs
@@ -0,0 +1,15 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace RepoStatusTable.DependencyInjection
+{
+	public static class Bindings
+	{
+		public static ServiceProvider CreateBindings()
+		{
+			return new ServiceProviderBuilder()
+				.ConfigureOptions()
+				.ConfigureServices()
+				.Build();
+		}
+	}
+}
---
+++ src/RepoStatusTable/DependencyInjection/OptionsBindings.cs
@@ -0,0 +1,82 @@
+using System;
+using System.IO;
+using System.Linq;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.Extensions.Options;
+using RepoStatusTable.Options;
+using RepoStatusTable.Options.CellProvider;
+using RepoStatusTable.Options.Validation;
+
+namespace RepoStatusTable.DependencyInjection
+{
+	public partial class ServiceProviderBuilder
+	{
+		public ServiceProviderBuilder ConfigureOptions()
+		{
+			var configurationRoot = ConfigureConfiguration();
+
+			AddOptionsSections( configurationRoot );
+			AddValidation();
+
+			return this;
+		}
+
+		private void AddOptionsSections( IConfiguration configurationRoot )
+		{
+			_collection.Configure<RepoOptions>( configurationRoot.GetSection( OptionsConstants.Repos ) );
+			_collection.Configure<TableViewOptions>( configurationRoot.GetSection( OptionsConstants.TableView ) );
+
+			// Cell providers
+			_collection.Configure<DirectoryNameProviderOptions>(
+				configurationRoot.GetSection( OptionsConstants.CellProviders.DirectoryNameProvider ) );
+			_collection.Configure<GitBranchProviderOptions>(
+				configurationRoot.GetSection( OptionsConstants.CellProviders.GitBranchProvider ) );
+			_collection.Configure<GitStatusProviderOptions>(
+				configurationRoot.GetSection( OptionsConstants.CellProviders.GitStatusProvider ) );
+		}
+
+		private void AddValidation()
+		{
+			_collection.TryAddEnumerable( ServiceDescriptor
+				.Singleton<IValidateOptions<RepoOptions>, RepoOptionsValidator>() );
+		}
+
+		private static IConfigurationRoot ConfigureConfiguration()
+		{
+			var configurationBuilder = new ConfigurationBuilder();
+
+			configurationBuilder.Sources.Clear();
+			configurationBuilder
+				.AddJsonFile( "config.json", true )
+				.AddJsonFile( GetAppDataDirConfigPath(), true )
+				.AddJsonFile( GetWorkingDirConfigPath(), true );
+
+			var configRoot = configurationBuilder.Build();
+
+			CheckConfigGiven( configRoot );
+
+			return configRoot;
+		}
+
+		private static void CheckConfigGiven( IConfiguration configRoot )
+		{
+			if ( configRoot.GetChildren().ToList().Any() ) return;
+
+			throw new ArgumentException( "No valid config given" );
+		}
+
+		private static string GetWorkingDirConfigPath()
+		{
+			var workingDir = Directory.GetCurrentDirectory();
+			return Path.Join( workingDir, "rstconfig.json" );
+		}
+
+		private static string GetAppDataDirConfigPath()
+		{
+			var appdataDir = Environment.GetFolderPath( Environment.SpecialFolder.ApplicationData );
+			return Path.Join( appdataDir, "RepoStatusTable", "config.json" );
+		}
+	}
+}
