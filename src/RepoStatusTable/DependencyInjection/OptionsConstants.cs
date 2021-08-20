ed4087ed0a4aac2777a1fb7cdd7f900e6e928186
ralphwcoggins <ralphwcoggins@gmail.com>
2021-08-20 22:18:40 +0200

    Split DI setup into options and service binding

---
+++ src/RepoStatusTable/DependencyInjection/OptionsConstants.cs
@@ -0,0 +1,16 @@
+namespace RepoStatusTable.DependencyInjection
+{
+	public static class OptionsConstants
+	{
+		public const string Repos = "Repos";
+		public const string TableView = "TableView";
+
+		public static class CellProviders
+		{
+			private const string Base = "CellProviders";
+			public static readonly string DirectoryNameProvider = $"{Base}:DirectoryNameProvider";
+			public static readonly string GitBranchProvider = $"{Base}:GitBranchProvider";
+			public static readonly string GitStatusProvider = $"{Base}:GitBranchProvider";
+		}
+	}
+}
---
+++ src/RepoStatusTable/DependencyInjection/ServiceBindings.cs
@@ -0,0 +1,46 @@
+using Microsoft.Extensions.DependencyInjection;
+using RepoStatusTable.CellProviders;
+using RepoStatusTable.Facade;
+using RepoStatusTable.Model;
+using RepoStatusTable.Utilities;
+using RepoStatusTable.View;
+using RepoStatusTable.View.SpectreConsoleTableView;
+
+namespace RepoStatusTable.DependencyInjection
+{
+	public partial class ServiceProviderBuilder
+	{
+		private readonly ServiceCollection _collection = new();
+
+		public ServiceProvider Build()
+		{
+			return _collection.BuildServiceProvider();
+		}
+
+		public ServiceProviderBuilder ConfigureServices()
+		{
+			_collection.AddSingleton<IApplication, Application>();
+
+			// Model and view
+			_collection.AddSingleton<ITableModel, TableModel>();
+			_collection.AddSingleton<ITableViewStrategy, SpectreConsoleTableViewLive>();
+			_collection.AddSingleton<ITableViewStrategy, SpectreConsoleTableViewDefault>();
+			_collection.AddSingleton<ITableView, TableViewProxy>();
+
+			// Facades
+			_collection.AddSingleton<IVcsFacade, GitFacade>();
+			_collection.AddSingleton<IGitFacade, GitFacade>();
+			_collection.AddSingleton<IFileSystemFacade, FileSystemFacade>();
+
+			// Cell Providers
+			_collection.AddSingleton<ICellProvider, DirectoryNameProvider>();
+			_collection.AddSingleton<ICellProvider, GitBranchProvider>();
+			_collection.AddSingleton<ICellProvider, GitStatusProvider>();
+
+			// Utilities
+			_collection.AddSingleton<IReposDirectoryUtility, ReposDirectoryUtility>();
+
+			return this;
+		}
+	}
+}
